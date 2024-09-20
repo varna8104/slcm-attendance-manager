154d4ab9ae9e402f471a610477f0157e74447e9c
varna8104 <varnaabhi8104@gmail.com>
2024-09-21 00:04:23 +0530

    tested previous logic with all test cases and made changes

--- slcmlogic.py
+++ slcmlogic.py
@@ -1,18 +1,18 @@
-tcc=int(input("Enter the no. of classes conducted:")) #tcc=total classes conducted
-tca=int(input("Enter the no. of classes attended:")) #tca=total classes attended
+tcc=int(input("Enter the number of classes conducted: ")) #tcc=total classes conducted
+tca=int(input("Enter the number of classes attended: ")) #tca=total classes attended
 if tca>tcc:
     print("Invalid input. Classes attended cannot be greater than classes conducted")
     exit()
 tctbc=100 #total classes to be conducted
 rc=(75*tctbc)/100 #required classes to maintain 75% attendence
-a=rc-tca #no of classes the student has to attend 
-rem=tctbc-tcc #no of classes that can be conducted
+a=rc-tca #number of classes the student has to attend 
+rem=tctbc-tcc #number of classes that can be conducted
 if tcc==tca==0:
     print("Your Current Attendence Percentage: 0")
 else:
-    print("Your Current Attendence Percentage:",round((tca/tcc)*100,2))
+    print("Your Current Attendence Percentage: ",round((tca/tcc)*100,2))
     if rem<a:
-        print("You have attendence shortage!! contact your HOD")
+        print("You have attendence shortage!! Contact your HOD")
     elif rem>a:
         print("You can afford to take leaves for",int(rem-a),"classes out of",rem,"classes")
     elif a==rem:
