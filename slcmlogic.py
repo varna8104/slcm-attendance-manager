86b34ee807a6026ef8da49de89f38bd2df6cd109
varna8104 <varnaabhi8104@gmail.com>
2024-09-20 23:56:58 +0530

    tested previous logic with all test cases and made changes

--- slcmlogic.py
+++ slcmlogic.py
@@ -1,12 +1,19 @@
-tcc=int(input("Enter the total number of classes conducted: "))
-tca=int(input("Enter the total number of classes attended: "))
-tctbc=100
-rc=int((75*tctbc)/100)
+tcc=int(input("Enter the no. of classes conducted:")) #tcc=total classes conducted
+tca=int(input("Enter the no. of classes attended:")) #tca=total classes attended
+if tca>tcc:
+    print("Invalid input. Classes attended cannot be greater than classes conducted")
+    exit()
+tctbc=100 #total classes to be conducted
+rc=(75*tctbc)/100 #required classes to maintain 75% attendence
 a=rc-tca #no of classes the student has to attend 
 rem=tctbc-tcc #no of classes that can be conducted
-if rem<a:
-    print("You have attendence shortage!! contact your HOD")
-elif rem>a:
-    print("Your current attendance Percentage is",(tca/tcc)*100,"\n You can bunk",rem-a,"classes out of",rem,"classes")
-elif a==rem:
-    print("You have to attend all the classes in the remaining",rem,"classes to maintain 75% attendence")
+if tcc==tca==0:
+    print("Your Current Attendence Percentage: 0")
+else:
+    print("Your Current Attendence Percentage:",round((tca/tcc)*100,2))
+    if rem<a:
+        print("You have attendence shortage!! contact your HOD")
+    elif rem>a:
+        print("You can afford to take leaves for",int(rem-a),"classes out of",rem,"classes")
+    elif a==rem:
+        print("You have to attend all the classes in the remaining",rem,"classes to maintain 75% attendence")
