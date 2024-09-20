441c7036692fcb631c6525fab120b42e293c5bb7
Surabhi222005 <rsdivyasurabhi@gmail.com>
2024-09-21 00:37:51 +0530

    added per variable

--- slcmlogic.py
+++ slcmlogic.py
@@ -1,3 +1,4 @@
+#basic logic
 tcc=int(input("Enter the number of classes conducted: ")) #tcc=total classes conducted
 tca=int(input("Enter the number of classes attended: ")) #tca=total classes attended
 if tca>tcc:
